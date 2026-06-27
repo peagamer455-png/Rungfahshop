ad8c5b68729d6ed67a51ade3ee1a9ea49385fa30
peagamer455-png <peagamer455@gmail.com>
2026-06-27 17:25:36 +0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,11 +21,12 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
         <div 
           className="fixed inset-0 bg-black/50 z-40 lg:hidden" 
           onClick={() => setIsOpen(false)}
+          style={{ pointerEvents: 'auto' }}
         />
       )}
 
       {/* Sidebar Container */}
-      <div className={`fixed top-0 left-0 h-full w-64 bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0`}>
+      <div className={`fixed top-0 left-0 h-full w-64 bg-white shadow-2xl z-[60] transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0`}>
         <div className="p-6">
           {/* Logo / Header */}
           <div className="flex items-center mb-8">
