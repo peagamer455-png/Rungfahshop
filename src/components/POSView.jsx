2d875c5e6994072d50af254493fcc5e392cc2bc1
peagamer455-png <peagamer455@gmail.com>
2026-07-03 11:20:43 +0700

    Update POSView.jsx

--- src/components/POSView.jsx
+++ src/components/POSView.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Header } from './SharedUI'; // Import จาก SharedUI
 import { formatCurrency } from '../utils';
 import SummaryCard from './SummaryCard';
