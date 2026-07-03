8d7623dfc56696f30783bc5dc39b239a8296547b
peagamer455-png <peagamer455@gmail.com>
2026-07-03 11:40:52 +0700

    Update POSView.jsx

--- src/components/POSView.jsx
+++ src/components/POSView.jsx
@@ -106,7 +106,7 @@ const POSView = ({ products, bills, loadData, setPopupContent, setShowPopup, nav
     setPopupContent({
         title: "📦 สรุปสินค้าที่ขายวันนี้",
         color: "green",
-        size: "xl",
+        size: "xxl",
         message: (
             <div className="max-h-[65vh] overflow-y-auto -mx-1 px-1">
                 {list.length === 0 ? (
